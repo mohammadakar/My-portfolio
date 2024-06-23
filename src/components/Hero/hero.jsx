d6c7a25ab1290617e37e2c9e1e977dc1dec07061
mohamad akar <MoeAkar123456@gmail.com>
2024-06-23 15:58:29 +0300

    adding my laravel project

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
             <div className={styles.content}>
                 <h1 className={styles.title}>Hi, I am Mohammad Akar</h1>
                 <p className={styles.description}>I am a full-stack developer with expertise in React, NodeJS, Express, and MongoDB,
-                        as well as proficiency in HTML, CSS, JavaScript, PHP, and MySQL. Feel free to reach out if you&apos;d like to learn more about my work!</p>
+                        as well as proficiency in HTML, CSS, JavaScript, PHP, Laravel and MySQL. Feel free to reach out if you&apos;d like to learn more about my work!</p>
                 <a href="mailto:moeakar123456@gmail.com" className={styles.contactBtn}>Contact me via email</a>
             </div>
             <img className={styles.heroImg} src={getImageUrl('hero/heroImage.JPG')} alt="image of me" />
